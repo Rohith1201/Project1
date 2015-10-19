d262e08d39520c941c3d81529795fa92fddb9f10
Krishna1997 <krishnachaitanyagudipati1997@gmail.com>
2015-10-20 02:27:44 +0530

    final dominos.cpp without documentation - krishna

--- cs251_base_code/src/dominos.cpp
+++ cs251_base_code/src/dominos.cpp
@@ -344,8 +344,9 @@ namespace cs251
       }
 
     }
-  
-   //The pulley system above spring
+
+
+    //The pulley system above spring
     {
       b2BodyDef *bd = new b2BodyDef;
       bd->type = b2_dynamicBody;
@@ -405,7 +406,6 @@ namespace cs251
       m_world->CreateJoint(myjoint);
 
     }
-
 
  // surface
 
@@ -467,7 +467,8 @@ namespace cs251
       b1->CreateFixture(fd1);
     }
    }
-   //The pulley system near wheels
+
+    //The pulley system near wheels
     {
       b2BodyDef *bd = new b2BodyDef;
       bd->type = b2_dynamicBody;
@@ -507,7 +508,9 @@ namespace cs251
       myjoint->Initialize(box1, box2, worldAnchorGround1, worldAnchorGround2, box1->GetWorldCenter(), box2->GetWorldCenter(), ratio);
       m_world->CreateJoint(myjoint);
     }
-   //bigpulley on left side
+
+
+    //bigpulley on left side
     {
      //The pulley system
 
@@ -586,7 +589,8 @@ namespace cs251
       m_world->CreateJoint(myjoint1);
     }
 
-	//spring
+
+    //spring
     {
       b2PolygonShape shape;
       shape.SetAsBox(2.5f, 0.2f);
@@ -735,8 +739,8 @@ namespace cs251
 	  b2Body* body = m_world->CreateBody(&bd);
 	  body->CreateFixture(&fd);
     }
-	
-     //lifting
+
+     // lifting
      {
       b2PolygonShape shape;
       shape.SetAsBox(1.0f, 0.1f);
@@ -772,6 +776,61 @@ namespace cs251
       }
      }
 
+     // wedge - seesaw system
+
+     {
+      //The triangle wedge
+      b2Body* sbody;
+      b2PolygonShape poly;
+      b2Vec2 vertices[3];
+      vertices[0].Set(-1,0);
+      vertices[1].Set(1,0);
+      vertices[2].Set(0,1.5);
+      poly.Set(vertices, 3);
+      b2FixtureDef wedgefd;
+      wedgefd.shape = &poly;
+      wedgefd.density = 10.0f;
+      wedgefd.friction = 0.0f;
+      wedgefd.restitution = 0.0f;
+      b2BodyDef wedgebd;
+      wedgebd.position.Set(-40.0f, 0.0f);
+      sbody = m_world->CreateBody(&wedgebd);
+      sbody->CreateFixture(&wedgefd);
+
+      //The plank on top of the wedge
+      b2PolygonShape shape;
+      shape.SetAsBox(6.0f, 0.2f);
+      b2BodyDef bd2;
+      bd2.position.Set(-40.0f, 1.5f);
+      bd2.type = b2_dynamicBody;
+      b2Body* body = m_world->CreateBody(&bd2);
+      b2FixtureDef *fd2 = new b2FixtureDef;
+      fd2->density = 1.f;
+      fd2->friction=1.0;
+      fd2->restitution=0.0;
+      fd2->shape = new b2PolygonShape;
+      fd2->shape = &shape;
+      body->CreateFixture(fd2);
+
+      b2RevoluteJointDef jd;
+      b2Vec2 anchor;
+      anchor.Set(-40.0f, 1.5f);
+      jd.Initialize(sbody, body, anchor);
+      m_world->CreateJoint(&jd);
+
+      //The light box on the right side of the see-saw
+      b2PolygonShape shape2;
+      shape2.SetAsBox(1.0f, 1.0f);
+      b2BodyDef bd3;
+      bd3.position.Set(-43.5f, 2.0f);
+      bd3.type = b2_dynamicBody;
+      b2Body* body3 = m_world->CreateBody(&bd3);
+      b2FixtureDef *fd3 = new b2FixtureDef;
+      fd3->density = 1.0f;fd3->friction=1.0;
+      fd3->shape = new b2PolygonShape;
+      fd3->shape = &shape2;
+      body3->CreateFixture(fd3);
+    }
 //skksm
   }
 
